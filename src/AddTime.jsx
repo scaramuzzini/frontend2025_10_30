2c44d2ee4a6803d3a22d2bcc42215095ad7f2815
Mario Torres <scaramuzzini@gmail.com>
2025-10-30 19:03:59 -0400

    add time post

--- src/AddTime.jsx
+++ src/AddTime.jsx
@@ -1,4 +1,9 @@
 import { useState } from "react";
+import axios from 'axios';
+
+const headers = {
+    "ngrok-skip-browser-warning": "123"
+}
 
 function AddTime() {
     const [nome,setNome] = useState('');
@@ -6,6 +11,19 @@ function AddTime() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(nome);
+        const novoTime = {
+            nome
+        }
+        axios.post('https://1394373742f7.ngrok-free.app/times', 
+            novoTime, {
+            headers:headers
+        })
+        .then(function(response) {
+            console.log(response);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
     };
 
     return (
